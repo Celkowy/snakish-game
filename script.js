848d31b84a4b28f881eaa15e7405933a0c423fe3
tucelek@gmail.com <tucelek@gmail.com>
2022-01-06 23:29:14 +0100

    playable game

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const enemyTable = []
 let result = 0
 let enemyInterval = 350
 const squareTable = []
-const yourTime = document.querySelector('.time')
-const startBoard = document.querySelector('.start-board')
-const board = document.querySelector('.board')
-const menu = document.querySelector('.menu')
+const YOUR_TIME = document.querySelector('.time')
+const START_BOARD = document.querySelector('.start-board')
+const BOARD = document.querySelector('.board')
+const MENU = document.querySelector('.menu')
 const SETTINGS = document.querySelector('.settings')
 const SETTINGS_POPUP = document.querySelector('.settings-popup')
 let show = true
@@ -21,7 +21,7 @@ let settings = {
 
 SETTINGS.addEventListener('click', () => {
   SETTINGS_POPUP.classList.toggle('hide')
-  startBoard.classList.toggle('hide')
+  START_BOARD.classList.toggle('hide')
   LEVEL_DIFFICULTY.textContent = `${settings.difficulty}`
   checkColor()
 })
@@ -41,8 +41,7 @@ function changeDifficulty(arg) {
   LEVEL_DIFFICULTY.textContent = `${settings.difficulty}`
   checkColor()
   SETTINGS_POPUP.classList.toggle('hide')
-  startBoard.classList.toggle('hide')
-  console.log(settings)
+  START_BOARD.classList.toggle('hide')
 }
 
 class Fields {
@@ -122,20 +121,19 @@ const gameMode = {
 const survival = () => {
   gameMode.mode = 'survival'
   SETTINGS.hidden = true
-  console.log('111')
-  startBoard.style.display = 'none'
-  board.style.display = 'flex'
+  START_BOARD.style.display = 'none'
+  BOARD.style.display = 'flex'
   player.square.style.display = 'block'
   startTimer()
   startGame(JSON.parse(localStorage.getItem('settings')).difficulty)
 }
 
 const collector = () => {
-  yourTime.innerHTML = `Time 00:25`
+  YOUR_TIME.innerHTML = `Time 00:25`
   gameMode.mode = 'collector'
   SETTINGS.hidden = true
-  startBoard.style.display = 'none'
-  board.style.display = 'flex'
+  START_BOARD.style.display = 'none'
+  BOARD.style.display = 'flex'
   player.square.style.display = 'block'
   startTimer()
   startGame(JSON.parse(localStorage.getItem('settings')).difficulty)
@@ -154,10 +152,10 @@ function startTimer() {
       if (minutes >= 5) timer.stop()
 
       if (seconds < 10) {
-        yourTime.innerText = `Time 0${timer.getTimeValues().minutes}:0${timer.getTimeValues().seconds}`
+        YOUR_TIME.innerText = `Time 0${timer.getTimeValues().minutes}:0${timer.getTimeValues().seconds}`
         return `0${minutes}:0${seconds}`
       } else {
-        yourTime.innerText = `Time 0${timer.getTimeValues().minutes}:${timer.getTimeValues().seconds}`
+        YOUR_TIME.innerText = `Time 0${timer.getTimeValues().minutes}:${timer.getTimeValues().seconds}`
         return `0${minutes}:${seconds}`
       }
     }
@@ -171,18 +169,16 @@ function startTimer() {
       if (seconds === 0) timer.stop()
 
       if (seconds < 10) {
-        yourTime.innerText = `Time 0${timer.getTimeValues().minutes}:0${timer.getTimeValues().seconds}`
+        YOUR_TIME.innerText = `Time 0${timer.getTimeValues().minutes}:0${timer.getTimeValues().seconds}`
         return `0${minutes}:0${seconds}`
       } else {
-        yourTime.innerText = `Time 0${timer.getTimeValues().minutes}:${timer.getTimeValues().seconds}`
+        YOUR_TIME.innerText = `Time 0${timer.getTimeValues().minutes}:${timer.getTimeValues().seconds}`
         return `0${minutes}:${seconds}`
       }
     }
   }
 }
-//poziomy trudności
 //restart jak przegrasz
-//napisać collector mode
 function startGame() {
   createEnemy = () => {
     while (true) {
@@ -196,7 +192,7 @@ function startGame() {
           break
         }
       } else {
-        if (yourTime.textContent === 'Time 00:00') {
+        if (YOUR_TIME.textContent === 'Time 00:00' || enemyTable.length >= 90) {
           timer.stop()
           player.square.style.display = 'none'
           show = false
@@ -227,7 +223,7 @@ function startGame() {
   }
 }
 
-menu.addEventListener('click', () => {
+MENU.addEventListener('click', () => {
   window.location.reload()
 })
 
@@ -240,9 +236,9 @@ function difficultyLevel(lv) {
     else if (level === 'hard') return 12
     else if (level === 'insane') return 8
   } else {
-    if (level === 'easy') return 350
-    else if (level === 'medium') return 300
-    else if (level === 'hard') return 250
-    else if (level === 'insane') return 150
+    if (level === 'easy') return 300
+    else if (level === 'medium') return 225
+    else if (level === 'hard') return 175
+    else if (level === 'insane') return 125
   }
 }
